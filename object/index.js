5b32ae3ca48f3f2a767dac0c6c398c858f387699
Dzmitry Vaitsiakhouski <vaitsiakhouskidzmitry@gmail.com>
2025-06-05 08:24:21 +0300

    first commit

---
+++ object/index.js
@@ -0,0 +1,14 @@
+let shedule = {}
+
+shedule["8:30"] = "get up";
+console.log(shedule);
+
+function isEmpty(n){
+  for (const key in n) {
+    if(key === undefined){   }
+    else{    }
+  }
+}
+
+isEmpty(shedule)
+
